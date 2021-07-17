4e82fb6104b7a2aff3a11e58e4f6740b44822745
Deen Gao <degao@ea.com>
2021-07-17 14:32:39 -0700

    chnage css

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -3,10 +3,12 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Tab3Page } from './tab3.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module';
+import { PointsComponent } from '../points/points.component';
 
 @NgModule({
   imports: [
@@ -14,9 +16,10 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     CommonModule,
     FormsModule,
     ExploreContainerComponentModule,
+    FontAwesomeModule,
     RouterModule.forChild([{ path: '', component: Tab3Page }]),
     Tab3PageRoutingModule,
   ],
-  declarations: [Tab3Page]
+  declarations: [Tab3Page, PointsComponent]
 })
 export class Tab3PageModule {}
